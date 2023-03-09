eb34b85f68542a6510fbf39ebf0ec910052685f9
katpyt <katarzyna.pytlik@gmail.com>
2023-03-09 20:48:41 +0100

    Create empty pages

---
+++ src/app/services/category.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CategoryModel } from '../models/category.model';
+
+@Injectable({ providedIn: 'root' })
+
+export class CategoryService {
+    constructor(private _httpClient: HttpClient) {
+    }
+
+    getAllCategories(): Observable<CategoryModel[]> {
+        return this._httpClient.get<CategoryModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-categories');
+    }
+
+    getOneCategory(categoryId: string): Observable<CategoryModel> {
+        return this._httpClient.get<CategoryModel>(`https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-categories/${categoryId}`);
+    }
+}
---
+++ src/app/services/store.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { StoreModel } from '../models/store.model';
+
+@Injectable({ providedIn: 'root' })
+
+export class StoreService {
+    constructor(private _httpClient: HttpClient) {
+    }
+
+    getAllStores(): Observable<StoreModel[]> {
+        return this._httpClient.get<StoreModel[]>('https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores');
+    }
+
+    getOneStore(storeId: string): Observable<StoreModel> {
+        return this._httpClient.get<StoreModel>(`https://6384fca14ce192ac60696c4b.mockapi.io/freshcart-stores/${storeId}`);
+    }
+}
