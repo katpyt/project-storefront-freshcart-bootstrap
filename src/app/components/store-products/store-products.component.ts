fedb6445d6baed03efb8ee3b8ae3d9f01692e83c
katpyt <katarzyna.pytlik@gmail.com>
2023-03-15 18:44:19 +0100

    store products page

--- src/app/components/store-products/store-products.component.ts
+++ src/app/components/store-products/store-products.component.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
-import { StoreModel } from '../../models/store.model';
+import { combineLatest, Observable } from 'rxjs';
+import { map, shareReplay, switchMap } from 'rxjs/operators';
+import { StoreQueryModel } from '../../queries/store.query-model';
+import { ProductModel } from '../../models/product.model';
 import { StoreService } from '../../services/store.service';
+import { ProductService } from '../../services/product.service';
+import helperFunctions from 'src/app/helperFunctions';
 
 @Component({
   selector: 'app-store-products',
@@ -13,10 +16,28 @@ import { StoreService } from '../../services/store.service';
 })
 
 export class StoreProductsComponent {
-  readonly storeDetails$: Observable<StoreModel> = this._activatedRoute.params.pipe(
-    switchMap(data => this._storeService.getOneStore(data['id']))
+  readonly storeDetails$: Observable<StoreQueryModel> = this._activatedRoute.params.pipe(
+    switchMap(data => this._storeService.getOneStore(data['storeId']))
+  ).pipe(
+    map((store) => {
+      return {
+        id: store.id,
+        name: store.name,
+        logoUrl: store.logoUrl.substring(1, store.logoUrl.length),
+        distanceInKilometers: helperFunctions.roundToOneDecimalPlaced(+store.distanceInMeters, 1) + " km",
+        tagIds: store.tagIds
+      }
+    }),
+    shareReplay(1)
   );
+  readonly storeProducts$: Observable<ProductModel[]> = combineLatest([
+    this._productService.getAllProducts(),
+    this._activatedRoute.params
+  ])
+    .pipe(
+      map(([products, params]) => products.filter(product => product.storeIds.includes(params['storeId'])))
+    );
 
-  constructor(private _activatedRoute: ActivatedRoute, private _storeService: StoreService) {
+  constructor(private _activatedRoute: ActivatedRoute, private _storeService: StoreService, private _productService: ProductService) {
   }
 }
